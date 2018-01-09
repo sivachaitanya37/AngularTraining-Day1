5bc330228b9dd2abb9c7077e7c6cac63aabb73e1
SivaChaitanya <sivachaitanya37@gmail.com>
2018-01-09 15:03:30 +0530

    Day2

--- ngproject1/src/app/app.module.ts
+++ ngproject1/src/app/app.module.ts
@@ -4,14 +4,22 @@ import { BrowserModule } from "@angular/platform-browser";
 import { HeaderComponent } from "./header.component";
 import { LoginModule } from "./login/login.module";
 import { LoginService } from "./services/login.service";
+import { HomeComponent } from "./shared/routes.components";
+import { RouterModule } from "@angular/router";
+import { SignInComponent } from "./login/sign-in.component";
+import { SignUpComponent } from "./login/sign-up.component";
 
 @NgModule({
     //Register component
-    declarations: [AppComponent, HeaderComponent],
+    declarations: [AppComponent, HeaderComponent, HomeComponent],
     //Startup component 
     bootstrap: [AppComponent],
     //Module dependencies
-    imports: [BrowserModule, LoginModule],
+    imports: [BrowserModule, LoginModule, RouterModule.forRoot([
+        { path: "", component: HomeComponent },
+        { path: "signin", component: SignInComponent },
+        { path: "signup", component: SignUpComponent }
+    ])],
     //Register Global Service
     providers: [LoginService]
 })
